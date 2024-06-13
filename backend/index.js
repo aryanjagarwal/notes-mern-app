4243e3ae313677b5d08469ab68b08549858b6380
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2024-06-13 19:52:25 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "*", // allow to server to accept request from different origin
+}));
+
+app.get('/', (req, res) => {
+    res.json({data: 'Hello World'});
+});
+
+app.listen(8000);
+
+module.exports = app;
