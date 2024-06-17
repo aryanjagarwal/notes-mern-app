0e87b3b781fbe145bace4aacc66fbfa8c022cb73
Aryan Jagarwal <aryanjagarwal@Aryans-MacBook-Air.local>
2024-06-17 10:08:53 +0530

    done

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,38 +1,51 @@
-import React, { useState } from 'react'
-import ProfileInfo from '../Cards/ProfileInfo'
-import { useNavigate } from 'react-router-dom'
-import SearchBar from '../SearchBar/SearchBar'
+import React, { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
-  const [searchQuery, setSearchQuery] = useState('');
-  const navigate = useNavigate;
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+  const isToken = localStorage.getItem("token");
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate();
 
   const onLogout = () => {
-    navigate("/helper");
+    localStorage.clear();
+    navigate("/login");
   };
 
   const handleSearch = () => {
-    //
+    if(searchQuery){
+      onSearchNote(searchQuery)
+    }
   };
 
-  const onClearSearch = () => {
-    setSearchQuery('');
-  };
+  const onClearSearch = ()=>{
+    handleClearSearch()
+    setSearchQuery("")
+  }
 
   return (
-    <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
-      <h2 className='text-xl font-medium text-black py-2'>Naughty Notes</h2>
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <SearchBar value={searchQuery}
-        onChange={({ target }) => {
-          setSearchQuery(target.value);
-        }}
-        handleSearch={handleSearch}
-        onClearSearch={onClearSearch} 
-      />
-      <ProfileInfo onLogout={onLogout} />
+      {isToken && (
+        <>
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => {
+              setSearchQuery(target.value);
+            }}
+            handleSearch={handleSearch}
+            onClearSearch={onClearSearch}
+          />
+
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
